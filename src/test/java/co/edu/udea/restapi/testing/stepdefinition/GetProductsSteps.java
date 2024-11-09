6e8e5f51119320657dac5d13201ebf0c32b6890a
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-11-08 22:48:46 -0500

    refactor: :recycle: Remove obsolete CreateProduct and GetProducts runners and steps

--- src/test/java/co/edu/udea/restapi/testing/stepdefinition/GetProductsSteps.java
+++ src/test/java/co/edu/udea/restapi/testing/stepdefinition/GetProductsSteps.java
@@ -5,6 +5,7 @@ import co.edu.udea.restapi.testing.questions.ErrorMessage;
 import co.edu.udea.restapi.testing.questions.StatusCode;
 import co.edu.udea.restapi.testing.tasks.ConnectTo;
 import co.edu.udea.restapi.testing.tasks.ConsumeThe;
+import com.google.gson.Gson;
 import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
@@ -20,9 +21,12 @@ import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeT
 import org.hamcrest.Matchers;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class GetProductsSteps {
+
+    private Map<String, Object> requestBody;
 
     Actor user = Actor.named("tae user");
 
@@ -37,17 +41,46 @@ public class GetProductsSteps {
         user.attemptsTo(ConnectTo.theService());
     }
 
+    @And("^the request body contains the following data$")
+    public void theRequestBodyContainsTheFollowingData(Map<String, Object> requestBody) {
+        Gson gson = new Gson();
+        List<String> images = gson.fromJson(requestBody.get("images").toString(), List.class);
+        Map<String, Object> requestBodyCopy = new HashMap<>(requestBody);
+        requestBodyCopy.put("images", images);
+        this.requestBody = requestBodyCopy;
+    }
+
+    @When("I send a POST request to {string}")
+    public void iSendAPOSTRequestTo(String arg0) {
+        user.attemptsTo(ConsumeThe.withBody(arg0, "POST", this.requestBody));
+    }
+
+    @When("I send a GET request to {string}")
+    public void iSendAGETRequestTo(String arg0) {
+        user.attemptsTo(ConsumeThe.service(arg0, "GET"));
+    }
+
+    @Then("the response status code should be {int}")
+    public void theResponseStatusCodeShouldBe(int arg0) {
+        user.should(seeThat("The response code is " + arg0, StatusCode.status(), Matchers.equalTo(arg0)));
+    }
+
+    @And("the response body should have a field {string} with value {string}")
+    public void theResponseBodyShouldHaveAFieldWithStringValue(String field, String value) {
+        user.should(seeThatResponse("the response body should have a field"+field, response -> response.body(field, Matchers.equalTo(value))));
+    }
+
+    @And("the response body should have a field {string} with value {int}")
+    public void theResponseBodyShouldHaveAFieldWithIntValue(String field, int value) {
+        user.should(seeThatResponse("the response body should have a field"+field, response -> response.body(field, Matchers.equalTo(value))));
+    }
+
     @When("I send a GET request to {string} with query parameters {string} set to {string} and {string} set to {string}")
     public void iSendAGETRequestToWithQueryParametersSetToAndSetTo(String arg0, String arg1, String arg2, String arg3, String arg4) {
         Map<String, String> params = new HashMap<>();
         params.put(arg1, arg2);
         params.put(arg3, arg4);
         user.attemptsTo(ConsumeThe.withParams(arg0, "GET", params));
-    }
-
-    @Then("the response status code should be {int}")
-    public void theResponseStatusCodeShouldBe(int arg0) {
-        user.should(seeThat("The response code is 200", StatusCode.status(), Matchers.equalTo(arg0)));
     }
 
     @And("the response body should contain a list of products")
