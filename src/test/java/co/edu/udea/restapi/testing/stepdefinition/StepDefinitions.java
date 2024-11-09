fb075ef66e9137865318922851fd3f230babeff9
Jose David Gómez <jose.gomez14@udea.edu.co>
2024-11-08 23:00:18 -0500

    refactor: :truck: Rename step definition class

--- src/test/java/co/edu/udea/restapi/testing/stepdefinition/StepDefinitions.java
+++ src/test/java/co/edu/udea/restapi/testing/stepdefinition/StepDefinitions.java
@@ -24,7 +24,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class GetProductsSteps {
+public class StepDefinitions {
 
     private Map<String, Object> requestBody;
 
